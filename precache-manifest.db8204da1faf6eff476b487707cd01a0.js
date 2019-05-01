d43b712f943458b26184f22e71a7db5577ece3f6
a-fuenzalida <a.fuenzalida1494@gmail.com>
2019-05-01 18:50:49 -0400

    Updates

---
+++ precache-manifest.db8204da1faf6eff476b487707cd01a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b607d7fd86a3c76a6ee9b121435ad64",
+    "url": "/promedio-notas/index.html"
+  },
+  {
+    "revision": "b5bfe1fc43e90f291640",
+    "url": "/promedio-notas/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "61089a8b4a74b3b3b2cd",
+    "url": "/promedio-notas/static/js/2.7bc42137.chunk.js"
+  },
+  {
+    "revision": "b5bfe1fc43e90f291640",
+    "url": "/promedio-notas/static/js/main.f3e07792.chunk.js"
+  },
+  {
+    "revision": "603cdd446197fe0e8702",
+    "url": "/promedio-notas/static/js/runtime~main.01d890e1.js"
+  }
+]);
